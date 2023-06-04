6bf92d1a27c62b1eb75f3828d53761f6692d4a20
t-mc4 <business.basics@aleeas.com>
2023-06-04 01:30:52 -0700

    Switched to pnpm

--- utils/scraping.js
+++ utils/scraping.js
@@ -5,32 +5,32 @@ const { searchPhoneNumbersInText } = require('libphonenumber-js');
 // import chromium from 'chrome-aws-lambda';
 
 export async function browseWebPage(url) {
-    let puppeteer;
-    let options;
+    // let puppeteer;
+    // let options;
 
-    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-        // running on the Vercel platform.
-        chrome = require('chrome-aws-lambda');
-        puppeteer = require('puppeteer-core');
-        options = {
-            args: [
-                ...chrome.args,
-                '--hide-scrollbars',
-                '--disable-web-security',
-            ],
-            defaultViewport: chrome.defaultViewport,
-            executablePath: await chrome.executablePath,
-            headless: 'new',
-            ignoreHTTPSErrors: true,
-        };
-    } else {
-        // running locally.
-        puppeteer = require('puppeteer');
-        options = { args: ['--no-sandbox'] };
-    }
+    // if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    //     // running on the Vercel platform.
+    //     chrome = require('chrome-aws-lambda');
+    //     puppeteer = require('puppeteer-core');
+    //     options = {
+    //         args: [
+    //             ...chrome.args,
+    //             '--hide-scrollbars',
+    //             '--disable-web-security',
+    //         ],
+    //         defaultViewport: chrome.defaultViewport,
+    //         executablePath: await chrome.executablePath,
+    //         headless: 'new',
+    //         ignoreHTTPSErrors: true,
+    //     };
+    // } else {
+    //     // running locally.
+    //     options = { args: ['--no-sandbox'] };
+    // }
+    let puppeteer = require('puppeteer');
 
-    // const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-    let browser = await puppeteer.launch(options);
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+    // let browser = await puppeteer.launch(options);
     const page = await browser.newPage();
     let content;
 
