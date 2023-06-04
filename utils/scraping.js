1fd32f4505ae8a32c49c37bfa19f7a5299706d84
t-mc4 <business.basics@aleeas.com>
2023-06-04 00:24:02 -0700

    Switched to yarn and refactored scraping.js to take into account aws lambda; two different setups depending on whether its running locally or on vercel/railway

--- utils/scraping.js
+++ utils/scraping.js
@@ -1,19 +1,37 @@
-import puppeteer from 'puppeteer';
-import * as cheerio from 'cheerio';
-import { searchPhoneNumbersInText } from 'libphonenumber-js';
+const cheerio = require('cheerio');
+const { searchPhoneNumbersInText } = require('libphonenumber-js');
+// import puppeteer from 'puppeteer';
 // import * as puppeteer from 'puppeteer';
 // import chromium from 'chrome-aws-lambda';
 
 export async function browseWebPage(url) {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+    let puppeteer;
+    let options;
+
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+        // running on the Vercel platform.
+        chrome = require('chrome-aws-lambda');
+        puppeteer = require('puppeteer-core');
+        options = {
+            args: [
+                ...chrome.args,
+                '--hide-scrollbars',
+                '--disable-web-security',
+            ],
+            defaultViewport: chrome.defaultViewport,
+            executablePath: await chrome.executablePath,
+            headless: true,
+            ignoreHTTPSErrors: true,
+        };
+    } else {
+        // running locally.
+        puppeteer = require('puppeteer');
+        options = {};
+    }
+
+    // const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+    let browser = await puppeteer.launch(options);
     const page = await browser.newPage();
-    // const browser = await chromium.puppeteer.launch({
-    //     args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-    //     defaultViewport: chromium.defaultViewport,
-    //     executablePath: await chromium.executablePath,
-    //     headless: true,
-    //     ignoreHTTPSErrors: true,
-    //   })
     let content;
 
     try {
