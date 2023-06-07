1d8ceac42bb030a1e767e62c1184e184eba124fe
Fredrick <95657057+fredriick@users.noreply.github.com>
2023-06-07 02:33:45 +0100

    Update scraping.js

--- utils/scraping.js
+++ utils/scraping.js
@@ -1,187 +1,105 @@
-// const cheerio = require('cheerio');
-// const { searchPhoneNumbersInText } = require('libphonenumber-js');
-// // import puppeteer from 'puppeteer';
-// // import * as puppeteer from 'puppeteer';
-// // import chromium from 'chrome-aws-lambda';
+const cheerio = require('cheerio');
+const { searchPhoneNumbersInText } = require('libphonenumber-js');
+// import puppeteer from 'puppeteer';
+// import * as puppeteer from 'puppeteer';
+// import chromium from 'chrome-aws-lambda';
 
-// process.setMaxListeners(15);
-
-// export async function browseWebPage(url) {
-//     let puppeteer;
-//     let options;
-
-//     if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-//         // running on the Vercel platform.
-//         chrome = require('chrome-aws-lambda');
-//         puppeteer = require('puppeteer-core');
-//         options = {
-//             args: [
-//                 ...chrome.args,
-//                 '--hide-scrollbars',
-//                 '--disable-web-security',
-//                 '--no-sandbox',
-//                 '--disable-setuid-sandbox',
-//             ],
-//             defaultViewport: chrome.defaultViewport,
-//             executablePath: await chrome.executablePath,
-//             headless: true,
-//             ignoreHTTPSErrors: true,
-//             ignoreDefaultArgs: ['--disable-extensions'],
-//         };
-//     } else {
-//         // running locally.
-//         puppeteer = require('puppeteer');
-//         options = { args: ['--no-sandbox'] };
-//     }
-
-//     const browser = await puppeteer.launch({ args: ['--no-sandbox'], headless: 'new' });
-//     // let browser = await puppeteer.launch(options);
-
-//     const page = await browser.newPage();
-//     let content;
-
-//     try {
-//         await page.goto(url, { waitUntil: 'networkidle0', timeout: 120000 });
-//         content = await page.content();
-//     } catch (error) {
-//         console.log(
-//             `Failed to load or process page: ${url} due to ${error.message} - substituting with empty text for speed`
-//         );
-//         return '';
-//     } finally {
-//         await browser.close();
-//     }
-
-//     return content;
-// }
-
-// export function removeDuplicates(phoneNumbers) {
-//     const uniquePhoneNumbers = [...new Set(phoneNumbers)];
-//     return uniquePhoneNumbers;
-// }
-
-// export async function findPhoneNumbersAndEmails(html) {
-//     const $ = cheerio.load(html);
-
-//     // Getting the text from the HTML
-//     let text = $('body').text();
-
-//     let phoneNumbers = [];
-
-//     for (const number of searchPhoneNumbersInText(text, 'US')) {
-//         console.log('Phone Number', number);
-//         await new Promise((resolve) => {
-//             setTimeout(resolve, 0);
-//             phoneNumbers.push(number.number.number);
-//         });
-//     }
-//     console.log('Finished');
-
-//     let commonDomains = [
-//         '.com',
-//         '.org',
-//         '.net',
-//         '.gov',
-//         '.edu',
-//         '.io',
-//         '.ai',
-//         '.co',
-//         '.me',
-//     ];
-//     let matches =
-//         text.match(
-//             /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+(?<!\.jpg|\.png|\.jpeg|\.gif)\.[A-Z|a-z]{2,}\b/g
-//         ) || [];
-//     let emails = matches.filter((email) =>
-//         commonDomains.some((domain) => email.includes(domain))
-//     );
-
-//     return {
-//         phoneNumbers: phoneNumbers || 'None',
-//         emails: emails,
-//     };
-// }
-import cheerio from 'cheerio';
-import { searchPhoneNumbersInText } from 'libphonenumber-js';
-import puppeteer from 'puppeteer';
+process.setMaxListeners(15);
 
 export async function browseWebPage(url) {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox'], headless: true });
-  const page = await browser.newPage();
-  let content;
+    let puppeteer;
+    let options;
 
-  try {
-    await page.goto(url, { waitUntil: 'networkidle0', timeout: 120000 });
-    content = await page.content();
-  } catch (error) {
-    console.log(`Failed to load or process page: ${url} due to ${error.message} - substituting with empty text for speed`);
-    return '';
-  } finally {
-    await browser.close();
-  }
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+        // running on the Vercel platform.
+        chrome = require('chrome-aws-lambda');
+        puppeteer = require('puppeteer-core');
+        options = {
+            args: [
+                ...chrome.args,
+                '--hide-scrollbars',
+                '--disable-web-security',
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+            ],
+            defaultViewport: chrome.defaultViewport,
+            executablePath: await chrome.executablePath,
+            headless: true,
+            ignoreHTTPSErrors: true,
+            ignoreDefaultArgs: ['--disable-extensions'],
+        };
+    } else {
+        // running locally.
+        puppeteer = require('puppeteer');
+        options = { args: ['--no-sandbox'] };
+    }
 
-  return content;
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'], headless: 'new' });
+    // let browser = await puppeteer.launch(options);
+
+    const page = await browser.newPage();
+    let content;
+
+    try {
+        await page.goto(url, { waitUntil: 'networkidle0', timeout: 120000 });
+        content = await page.content();
+    } catch (error) {
+        console.log(
+            `Failed to load or process page: ${url} due to ${error.message} - substituting with empty text for speed`
+        );
+        return '';
+    } finally {
+        await browser.close();
+    }
+
+    return content;
 }
 
 export function removeDuplicates(phoneNumbers) {
-  const uniquePhoneNumbers = [...new Set(phoneNumbers)];
-  return uniquePhoneNumbers;
+    const uniquePhoneNumbers = [...new Set(phoneNumbers)];
+    return uniquePhoneNumbers;
 }
 
 export async function findPhoneNumbersAndEmails(html) {
-  const $ = cheerio.load(html);
-  const text = $('body').text();
-  const phoneNumbers = [];
+    const $ = cheerio.load(html);
 
-  for (const number of searchPhoneNumbersInText(text, 'US')) {
-    console.log('Phone Number:', number);
-    await new Promise((resolve) => {
-      setTimeout(() => {
-        phoneNumbers.push(number.number.number);
-        resolve();
-      }, 0);
-    });
-  }
+    // Getting the text from the HTML
+    let text = $('body').text();
 
-  console.log('Finished');
+    let phoneNumbers = [];
 
-  const commonDomains = [
-    '.com',
-    '.org',
-    '.net',
-    '.gov',
-    '.edu',
-    '.io',
-    '.ai',
-    '.co',
-    '.me',
-  ];
+    for (const number of searchPhoneNumbersInText(text, 'US')) {
+        console.log('Phone Number', number);
+        await new Promise((resolve) => {
+            setTimeout(resolve, 0);
+            phoneNumbers.push(number.number.number);
+        });
+    }
+    console.log('Finished');
 
-  const matches = text.match(
-    /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+(?<!\.jpg|\.png|\.jpeg|\.gif)\.[A-Z|a-z]{2,}\b/g
-  ) || [];
+    let commonDomains = [
+        '.com',
+        '.org',
+        '.net',
+        '.gov',
+        '.edu',
+        '.io',
+        '.ai',
+        '.co',
+        '.me',
+    ];
+    let matches =
+        text.match(
+            /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+(?<!\.jpg|\.png|\.jpeg|\.gif)\.[A-Z|a-z]{2,}\b/g
+        ) || [];
+    let emails = matches.filter((email) =>
+        commonDomains.some((domain) => email.includes(domain))
+    );
 
-  const emails = matches.filter((email) =>
-    commonDomains.some((domain) => email.includes(domain))
-  );
-
-  return {
-    phoneNumbers: phoneNumbers.length > 0 ? phoneNumbers : 'None',
-    emails,
-  };
+    return {
+        phoneNumbers: phoneNumbers || 'None',
+        emails: emails,
+    };
 }
 
-export async function processWebPage(url) {
-  try {
-    const html = await browseWebPage(url);
-    const { phoneNumbers, emails } = await findPhoneNumbersAndEmails(html);
-    console.log('Phone Numbers:', phoneNumbers);
-    console.log('Emails:', emails);
-  } catch (error) {
-    console.error('Error:', error);
-  }
-}
-
-// Usage Example
-processWebPage('http://example.com');
 
